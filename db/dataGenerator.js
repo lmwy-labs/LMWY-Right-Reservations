49028313e14afdca36db47b12981875b08f1de71
ez-li <eileenli.sf@gmail.com>
2019-08-26 20:01:18 -0700

    Refactoring data gen to specify number of days data to generate

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -11,37 +11,52 @@ connection.connect(function(err) {
     console.log('connected to mysql');
 });
 
-const generateReservations = (cb) => {
-    var reservations = [];
-    const time_hour_min = [0, .5]
-    for (var i = 1; i < 101; i ++) {
-        const name = 'r' + i;
-        const max_seats = Math.floor(Math.random()*(100-30)+30);
-        const time_opening = (Math.floor(Math.random()*(19-17)+17) + time_hour_min[Math.floor(Math.random()*2)]);
-        const time_closing = (Math.floor(Math.random()*(24-21)+21) + time_hour_min[Math.floor(Math.random()*2)]);
-        const startDate = new Date('09/03/2019');
-        for (var j = 0; j < 60; j ++) {
-            var newDate = new Date(startDate.setDate(startDate.getDate() + 1));
-            // generate 30min interval slots
-            for (var k = time_opening; k < time_closing; k += .5) {
-                // generate random # open and reserved seats
-                var num_reserved_seats = Math.floor(Math.random()*(max_seats*.2) + (max_seats*.8));
-                var num_open_seats = max_seats - num_reserved_seats;
-                var reservation = [name, time_opening*10000, time_closing*10000, newDate, k*10000, num_open_seats, num_reserved_seats];
-                reservations.push(reservation);
+const generateReservations = (numOfMonths, cb) => {
+
+    const generateByMonth = (startDate) => {
+        var reservations = [];
+        const time_hour_min = [0, .5];
+        for (var i = 1; i < 101; i ++) {
+            const name = 'r' + i;
+            const max_seats = Math.floor(Math.random()*(100-30)+30);
+            const time_opening = (Math.floor(Math.random()*(19-11)+11) + time_hour_min[Math.floor(Math.random()*2)]);
+            const time_closing = (Math.floor(Math.random()*(24-21)+21) + time_hour_min[Math.floor(Math.random()*2)]);
+            var newDate = new Date(startDate);
+            for (var j = 0; j < 30; j ++) {
+                if (j === 0) {
+                    newDate = new Date(startDate);
+                } else {
+                    newDate = new Date(newDate.setDate(newDate.getDate() + 1));
+                }
+                // generate 30min interval slots
+                for (var k = time_opening; k < time_closing; k += .5) {
+                    // generate random # open and reserved seats
+                    var num_reserved_seats = Math.floor(Math.random()*(max_seats*.2) + (max_seats*.8));
+                    var num_open_seats = max_seats - num_reserved_seats;
+                    var reservation = [name, time_opening*10000, time_closing*10000, newDate, k*10000, num_open_seats, num_reserved_seats];
+                    reservations.push(reservation);
+                }
             }
         }
+        connection.query({ sql: 'INSERT INTO reservations (name, time_opening, time_closing, calendar_date, calendar_time, num_open_seats, num_reserved_seats) VALUES ?', values: [reservations] }, (err, results, fields) => {
+            if (err) return console.log(err);
+            cb(null, 'Generated Reservations');
+        });
     }
-    connection.query({ sql: 'INSERT INTO reservations (name, time_opening, time_closing, calendar_date, calendar_time, num_open_seats, num_reserved_seats) VALUES ?', values: [reservations] }, (err, results, fields) => {
-        if (err) return console.log(err);
-        cb(null, 'Generated Reservations');
-    });
+
+    const startDate = new Date('08/03/2019');
+    var newDate = new Date(startDate);
+    for (var x = 0; x < numOfMonths; x ++) {
+        new Date(newDate.setDate(newDate.getDate() + 30));
+        generateByMonth(newDate);
+    }
 }
 
-generateReservations((err, data) => {
-        if (err) return console.log(err);
-        console.log(data);
+generateReservations(3, (err, message) => {
+    if (err) return console.log(err);
+    console.log(message);
 });
+
 
 const getReservation = (rname, date, cb) => {
     var data = [rname, date];
