fd442dde95016cc82198fcb409a16a1903dff39b
TK <taehnkim@gmail.com>
2019-09-14 11:11:46 -0700

    Added reservations seeder

--- db/postgres/dbHelpers.js
+++ db/postgres/dbHelpers.js
@@ -5,6 +5,7 @@
 
 // RESERVATIONS
 // set restaurant reserved capacity at 30-80%
+// 1 week - 09-15 thru 9/21
 
 const randomInt= (min = 0, max) => {
   // generate random int between min and max included 
@@ -19,6 +20,17 @@ const createRestaurant = (id) => {
   return `${name}, ${open_time}, ${close_time}, ${capacity_per_slot}\n`;
 };
 
+const createReservation = () => {
+  let restaurant_id = randomInt(1, 10000000);
+  let reservation_date = '09-' + randomInt(15, 21) + '-2019';
+  let reservation_time = ('0' + randomInt(9, 18)).slice(-2) + ':00'
+  let reservation_datetime = reservation_date + ' ' + reservation_time;
+  let seats = randomInt(2, 5);
+
+  return `${restaurant_id}, ${reservation_datetime}, ${seats}\n`;
+};
+
 module.exports = {
   createRestaurant,
+  createReservation,
 }
--- db/postgres/seeder.js
+++ db/postgres/seeder.js
@@ -3,13 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const csv = require('fast-csv');
 
-const ws = fs.createWriteStream(path.join(__dirname, '../', 'csv/restaurants.csv'));
-// writeUsers.write('id,name,open_time,close_time,capacity_per_slot\n', 'utf8');
-ws.on('finish', function(){
-  console.log('DONE!');
-});
+const ws_restaurant = fs.createWriteStream(path.join(__dirname, '../', 'csv/restaurants.csv'));
+const ws_reservation = fs.createWriteStream(path.join(__dirname, '../', 'csv/reservations.csv'));
 
-const createRestaurants = (n, writer, encoding, callback) => {
+const createData = (func, n, writer, encoding, callback) => {
   let i = n;
   let id = 0;
   function write() {
@@ -17,7 +14,7 @@ const createRestaurants = (n, writer, encoding, callback) => {
     do {
       i -= 1;
       id += 1;
-      let data = helper.createRestaurant(id);
+      let data = func(id);
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
@@ -31,7 +28,16 @@ const createRestaurants = (n, writer, encoding, callback) => {
   write();
 }
 
-createRestaurants(10000000, ws, 'utf-8', () => {
-  ws.end();
+createData(helper.createRestaurant, 10000000, ws_restaurant, 'utf-8', () => {
+  ws_restaurant.end();
+});
+ws_restaurant.on('finish', function(){
+  console.log('Finished seeding restaurants.');
 });
 
+createData(helper.createReservation, 100000000, ws_reservation, 'utf-8', () => {
+  ws_reservation.end();
+});
+ws_reservation.on('finish', function(){
+  console.log('Finished seeding reservations.');
+});
